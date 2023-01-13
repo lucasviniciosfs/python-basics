fc996c36b90f210289b90edf80a5d1ea083cf6ea
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-01-13 16:52:46 +0000

    list comprehension 2

--- first.py
+++ first.py
@@ -1,7 +1,10 @@
-answer = [ name[0] for name in ["Elie", "Tim", "Matt"] ]
-print(answer)
+list1 = [1,2,3,4]
+list2 = [3,4,5,6]
 
-list = [1,2,3,4,5,6]
+answer = [num for num in list2 if num in list1]
 
-answer2 = [ even for even in list if even % 2 == 0 ]
+list3 = ["Elie", "Tim", "Matt"]
+
+answer2 = [name[::-1].lower() for name in list3]
+
 print(answer2)
