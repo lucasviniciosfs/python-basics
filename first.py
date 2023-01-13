16e740777bc6ee8a701933c994c60f1b2f5ce36c
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-01-13 16:53:25 +0000

    list comprehension 3

--- first.py
+++ first.py
@@ -1,10 +1,3 @@
-list1 = [1,2,3,4]
-list2 = [3,4,5,6]
+answer = [ [num for num in range(0,3)] for numa in range(0,3) ]
 
-answer = [num for num in list2 if num in list1]
-
-list3 = ["Elie", "Tim", "Matt"]
-
-answer2 = [name[::-1].lower() for name in list3]
-
-print(answer2)
+print(answer);
