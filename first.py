580fa179eef9f77410ee398d0a03cc5a2c688d59
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-01-13 16:52:18 +0000

    list comprehension

--- first.py
+++ first.py
@@ -1,1 +1,7 @@
-print("Hello!")
+answer = [ name[0] for name in ["Elie", "Tim", "Matt"] ]
+print(answer)
+
+list = [1,2,3,4,5,6]
+
+answer2 = [ even for even in list if even % 2 == 0 ]
+print(answer2)
