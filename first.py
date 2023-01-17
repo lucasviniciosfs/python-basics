dae4a346ee56cf35ee3245fee874678ad6a077b6
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-01-17 16:12:21 +0000

    partition function

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+first_list = []
+second_list = []
+
+def isEven(num):
+    return num % 2 == 0
+
+def partition(list, fn):
+    for item in list:
+        if fn(item):
+            global first_list
+            first_list.append(item)
+        else:
+            global second_list
+            second_list.append(item)
+    return [first_list, second_list]
+            
+print(partition([1,2,3,4], isEven))
