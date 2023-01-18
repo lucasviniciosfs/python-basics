fb907e8b8d2d7fff98a4fef24778586688b2bdbd
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-01-18 11:21:14 +0000

    map lambda

--- first.py
+++ first.py
@@ -1,17 +1,4 @@
-first_list = []
-second_list = []
+def decrement_list(nums):
+    return list(map(lambda num: num - 1, nums))
 
-def isEven(num):
-    return num % 2 == 0
-
-def partition(list, fn):
-    for item in list:
-        if fn(item):
-            global first_list
-            first_list.append(item)
-        else:
-            global second_list
-            second_list.append(item)
-    return [first_list, second_list]
-            
-print(partition([1,2,3,4], isEven))
+print(decrement_list([1,2,3]))
