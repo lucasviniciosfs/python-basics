661a81294ad977fa4baee007fdc1737e1a17d3c9
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-01-18 11:47:59 +0000

    all function

--- first.py
+++ first.py
@@ -1,4 +1,5 @@
-def decrement_list(nums):
-    return list(map(lambda num: num - 1, nums))
+def is_all_strings(list):
+    return all(type(item) == str for item in list)
 
-print(decrement_list([1,2,3]))
+print(is_all_strings(['a', 'b', 'c']))
+print(is_all_strings([2,'a', 'b', 'c']))
