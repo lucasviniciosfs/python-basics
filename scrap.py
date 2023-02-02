720c3e54ecc86a6358980f42a3d7b52a1240ce1a
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-02-02 14:38:41 +0000

    scapping

---
+++ scrap.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Quote:
+    def __init__(self, text, name, link):
+        self.text = text
+        self.name = name      
+        self.link = link
+
+request = requests.get("http://quotes.toscrape.com")
+html = BeautifulSoup(request.text, "html.parser")
+quotes = []
+for quote in html.select(".quote"):
+    quotes.append(
+        Quote(
+          quote.select(".text")[0].getText(),
+          quote.select("[itemprop='author']")[0].getText(),
+           quote.select("[itemprop='author']")[0].findNextSibling()["href"]
+        )
+    )
+
+print(quotes[0].name)
+
+
