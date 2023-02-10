91b0490e4b456f7ca1da649333c84d99f6038f9a
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-02-10 18:07:33 +0000

    find_factors

--- exercises.py
+++ exercises.py
@@ -1,9 +1,7 @@
-def titleize(text):
-    textStr = ''.join(text)
-    words = textStr.split(' ')
-    for word in words:
-        words[words.index(word)] = word.replace(word[0], word[0].upper())
-    return ' '.join(words)
+def find_factors(num):
+    return [ n for n in range(1,num+1) if num % n == 0]
 
-print(titleize('this is awesome'))
-print(titleize('oNLy cAPITALIZe fIRSt')) 
+print(find_factors(10)) # [1,2,5,10 ]
+print(find_factors(11)) # [1,11]
+print(find_factors(111)) # [1,3,37,111 ]
+print(find_factors(321421)) # [1,293,1097,321421 ]
