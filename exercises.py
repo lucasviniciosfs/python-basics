90db69cb248fa67a09e1b467c25f3990547435fb
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-02-13 10:02:55 +0000

    repeat function

--- exercises.py
+++ exercises.py
@@ -1,7 +1,11 @@
-def find_factors(num):
-    return [ n for n in range(1,num+1) if num % n == 0]
+def repeat(text, repetitions):
+    finalText = ''
+    while repetitions > 0:
+        finalText += text
+        repetitions -= 1
+    return finalText
 
-print(find_factors(10)) # [1,2,5,10 ]
-print(find_factors(11)) # [1,11]
-print(find_factors(111)) # [1,3,37,111 ]
-print(find_factors(321421)) # [1,293,1097,321421 ]
+
+print(repeat('*', 3))  # '***' 
+print(repeat('abc', 2))  # 'abcabc' 
+print(repeat('abc', 0))  # ''
