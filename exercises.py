ccfd4e87f961457dd83503a0bab034eded4a134d
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-02-14 14:46:15 +0000

    is_odd_string

--- exercises.py
+++ exercises.py
@@ -1,13 +1,14 @@
-def find_greater_numbers(lista):
-    count = 0
-    for i in range(0, len(lista)):
-        for item in lista[i:]:
-            #print(lista[i], item)
-            if lista[i] < item:
-                count +=1
-    return count
+alphabet = {chr(i+96):i for i in range(1,27)}
 
-print(find_greater_numbers([1,2,3])) # 3 
-print(find_greater_numbers([6,1,2,7])) # 4
-print(find_greater_numbers([5,4,3,2,1])) # 0
-print(find_greater_numbers([])) # 0
+def is_odd_string(text):
+    sum = 0
+    for char in text:
+        sum += dict(alphabet).get(char)
+    return False if sum % 2 == 0 else True
+
+
+print(is_odd_string('a')) # True
+print(is_odd_string('aaaa')) # False
+print(is_odd_string('amazing')) # True
+print(is_odd_string('veryfun')) # True
+print(is_odd_string('veryfunny')) # False
