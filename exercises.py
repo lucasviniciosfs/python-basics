8fb16c49d56c57790bdcde26c86adefb2b171702
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-02-14 10:22:28 +0000

    sum NxN list of lists

--- exercises.py
+++ exercises.py
@@ -1,10 +1,41 @@
-def range_in_list(lista, start=0, end=-1):
-    if end == -1:
-        end = len(lista)
-    return lista[-1]
+def sum_up_diagonals(listas):
+  posInicial = 0
+  posFinal = len(listas) - 1
+  sum_diag = 0
+  for lista in listas:
+    sum_diag += lista[posInicial] + lista[posFinal]
+    posInicial += 1
+    posFinal -= 1
+  return sum_diag
 
-print(range_in_list([1,2,3,4],0,2)) #  6
-print(range_in_list([1,2,3,4],0,3)) # 10
-print(range_in_list([1,2,3,4],1)) #  9
-print(range_in_list([1,2,3,4])) # 10
-print(range_in_list([1,2,3,4],0,100)) # 10
+list1 = [
+  [ 1, 2 ],
+  [ 3, 4 ]
+]
+ 
+print(sum_up_diagonals(list1))
+
+list2 = [
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ]
+]
+ 
+print(sum_up_diagonals(list2))  # 30
+ 
+list3 = [
+  [ 4, 1, 0 ],
+  [ -1, -1, 0],
+  [ 0, 0, 9]
+]
+
+print(sum_up_diagonals(list3))  # 11
+
+list4 = [
+  [ 1, 2, 3, 4 ],
+  [ 5, 6, 7, 8 ],
+  [ 9, 10, 11, 12 ],
+  [ 13, 14, 15, 16 ]
+]
+ 
+print(sum_up_diagonals(list4))  # 68
