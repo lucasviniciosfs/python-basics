f944b50119229a6c934f4438f95e9421ff312993
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-02-14 10:50:36 +0000

    dictionary, min and max

--- exercises.py
+++ exercises.py
@@ -1,42 +1,5 @@
-def sum_up_diagonals(listas):
-  pos = 0
-  sum_diag = 0
-  for lista in listas:
-    sum_diag += lista[pos]
-    pos += 1
-  for lista in listas:
-    pos -= 1
-    sum_diag += lista[pos]
-  return sum_diag
+def min_max_key_in_dictionary(dic):
+    return [min(dict(dic).keys()), max(dict(dic).keys())]
 
-list1 = [
-  [ 1, 2 ],
-  [ 3, 4 ]
-]
- 
-print(sum_up_diagonals(list1))
-
-list2 = [
-  [ 1, 2, 3 ],
-  [ 4, 5, 6 ],
-  [ 7, 8, 9 ]
-]
- 
-print(sum_up_diagonals(list2))  # 30
- 
-list3 = [
-  [ 4, 1, 0 ],
-  [ -1, -1, 0],
-  [ 0, 0, 9]
-]
-
-print(sum_up_diagonals(list3))  # 11
-
-list4 = [
-  [ 1, 2, 3, 4 ],
-  [ 5, 6, 7, 8 ],
-  [ 9, 10, 11, 12 ],
-  [ 13, 14, 15, 16 ]
-]
- 
-print(sum_up_diagonals(list4))  # 68
+print(min_max_key_in_dictionary({2:'a', 7:'b', 1:'c',10:'d',4:'e'})) # [1,10]
+print(min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"})) # [1,4]
