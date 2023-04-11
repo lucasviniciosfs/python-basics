0e63a98440d8b2ca2c6c9bd183e6d00ed295fad5
Lucas Vinicios <lucasvinicios-fs@hotmail.com>
2023-04-11 18:51:59 +0100

    Deep Learning

--- machine-deep-learning.py
+++ machine-deep-learning.py
@@ -1,33 +1,15 @@
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-import pandas as pa
-from keras.optimizers import Adam
 
-data = pa.read_csv('credit_data.csv')
-
-features = data[['income','age','loan']]
-labels = np.array(data.default).reshape(-1, 1)
-
-encoder = OneHotEncoder()
-targets = encoder.fit_transform(labels).toarray()
-
-features_train, features_test, targets_train, targets_test = train_test_split(features, targets, train_size=0.2)
+features = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], 'float32')
+targets = np.array([[0], [1], [1], [0]], 'float32')
 
 model = Sequential()
-model.add(Dense(10, input_dim = 3, activation="sigmoid"))
-model.add(Dense(2, activation='softmax'))
-
-optimizer = Adam(0.001)
-model.compile(
-    loss='categorical_crossentropy',
-    optimizer=optimizer,
-    metrics=['accuracy']
-)
-
-model.fit(features_train, targets_train, epochs=10000, verbose=2)
-result = model.evaluate(features_test, targets_test, use_multiprocessing=True)
-
-print(result)
+model.add(Dense(2, input_dim=16, activation='relu'))
+model.add(Dense(16, input_dim=16, activation='relu'))
+model.add(Dense(16, input_dim=16, activation='relu'))
+model.add(Dense(16, input_dim=16, activation='relu'))
+model.add(Dense(16, input_dim=16, activation='relu'))
+model.add(Dense(16, input_dim=16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
